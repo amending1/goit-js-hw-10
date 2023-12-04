64bf97d567c889f68b49e41fe68d4efb18dad551
amending1 <joasia.sowiak@gmail.com>
2023-12-04 20:03:46 +0100

    error and loader fixed

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,25 +3,13 @@ axios.defaults.headers.common['x-api-key'] =
   'live_6iBgCXZRlrmB01CH5KRgYAQ7uz4Rs6J14X5chxd4DcenYSIATPoSJzkRfPujPiVS';
 
 export function fetchBreeds() {
-  axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(function (response) {
-      const cats = response.data; //tablica
-      const select = document.querySelector('.breed-select');
-      cats.forEach(function (entry) {
-        select.options.add(new Option(entry.name, entry.id));
-      });
-      console.log(cats);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .finally(function () {});
+  const promise = axios.get('https://api.thecatapi.com/v1/breeds');
+  return promise;
 }
 
 export function fetchCatByBreed(breedId) {
-  const promise = axios.get(
+  const promiseBreedId = axios.get(
     `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
   );
-  return promise;
+  return promiseBreedId;
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 
-fetchBreeds();
 // const select = new SlimSelect({
 //   select: '.breed-select',
 // });
 const select = document.querySelector('.breed-select');
 const catInfoDiv = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
+const loaderAnimation = document.querySelector('span.loader-animation');
 const errorParagraph = document.querySelector('.error');
+
+hideError();
+
+fetchBreeds()
+  .then(function (response) {
+    const cats = response.data; //tablica
+    const select = document.querySelector('.breed-select');
+    cats.forEach(function (entry) {
+      select.options.add(new Option(entry.name, entry.id));
+    });
+    console.log(cats);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    hideLoader();
+  });
 
 select.addEventListener('change', event => {
   const catId = select.options[select.selectedIndex].value;
@@ -44,18 +63,23 @@ function showLoader() {
   select.style.display = 'none';
   catInfoDiv.style.display = 'none';
   loader.style.display = 'block';
+  loaderAnimation.style.display = 'block';
   errorParagraph.style.display = 'none';
 }
 
 function hideLoader() {
   loader.style.display = 'none';
+  loaderAnimation.style.display = 'none';
 }
 
 function showError() {
   select.style.display = 'none';
   catInfoDiv.style.display = 'none';
   loader.style.display = 'none';
-  errorParagraph.style.display = 'block';
+  // errorParagraph.style.display = 'block';
+  Notiflix.Notify.warning(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
 }
 
 function hideError() {
